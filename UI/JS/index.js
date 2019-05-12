0bf8c764c5bf38c447eb674d12f21196d1d2206f
emmy <gmemmy22@gmail.com>
2019-05-12 23:48:15 +0100

    [Feature #165958512] create navigation menu for user feed page

--- UI/JS/index.js
+++ UI/JS/index.js
@@ -1,9 +1,9 @@
 // Get the modal
-const modal = document.getElementById('id01');
+const modal = document.querySelector('#id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.addEventListener ('click', event => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+});
