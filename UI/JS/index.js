f64548901db1a2674729dd235fb5331d5de19a22
emmy <gmemmy22@gmail.com>
2019-05-13 13:45:42 +0100

    [Chore #165958512] Develop modal advert cards for user feed page

--- UI/JS/index.js
+++ UI/JS/index.js
@@ -1,5 +1,5 @@
 // Get the modal
-const modal = document.querySelector('#id01');
+const modal = document.querySelector('#id01', );
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener ('click', event => {
@@ -7,3 +7,31 @@ window.addEventListener ('click', event => {
         modal.style.display = "none";
     }
 });
+
+
+//Modal setup for advertisments card
+const cardModal = document.getElementById('boxModal');
+
+//Get the button that opens the modal
+const btn = document.querySelector("#adcard","#adcard2");
+
+//Get the span element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+//When the user clicks on the card, open the modal
+btn.onclick = () => {
+	cardModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+	cardModal.style.display = "none";
+}
+
+//When the user clicks anywhere outside of the modal, clos it
+window.onclick = (event) => {
+	if (event.target == cardModal) {
+		cardModal.style.display = "none";
+	}
+}
+
