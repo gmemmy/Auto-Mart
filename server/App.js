07a16dd187176a53e889a3b512c2190f1ef28646
gmemmy <gmemmy22@gmail.com>
2019-05-24 03:11:01 +0100

    #166228597 Setup-Project-Tools (#33)
    * [Chore #166228597] Setup tools for project
    
    * [Chore #166228597] Setup babel in project

---
+++ server/App.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+ console.log(`server is listening on port ${port}!`)
+})
+
+export default app;
