b135359b3076eb83965db8de55b4d2b2891b9197
emmy <gmemmy22@gmail.com>
2019-06-01 14:44:29 +0100

    [Feature #166230139] Create API that lets users post a new car sale advertisement

--- UI/JS/index.js
+++ UI/JS/index.js
@@ -1,37 +1,42 @@
 // Get the modal
-const modal = document.querySelector('#id01', );
+// eslint-disable-next-line no-undef
+const modal = document.querySelector('#id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener ('click', event => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+// eslint-disable-next-line no-undef
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
 });
 
 
-//Modal setup for advertisments card
+// Modal setup for advertisments card
+// eslint-disable-next-line no-undef
 const cardModal = document.querySelector('#boxModal');
 
-//Get the button that opens the modal
+// Get the button that opens the modal
+// eslint-disable-next-line no-undef
 const btn = document.querySelector('#adcard');
 
-//Get the span element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// Get the span element that closes the modal
+// eslint-disable-next-line no-undef
+const span = document.getElementsByClassName('close')[0];
 
-//When the user clicks on the card, open the modal
+// When the user clicks on the card, open the modal
 btn.onclick = () => {
-	cardModal.style.display = "block";
-}
+  cardModal.style.display = 'block';
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = () => {
-	cardModal.style.display = "none";
-}
+  cardModal.style.display = 'none';
+};
 
-//When the user clicks anywhere outside of the modal, clos it
+// When the user clicks anywhere outside of the modal, clos it
+// eslint-disable-next-line no-undef
 window.onclick = (event) => {
-	if (event.target == cardModal) {
-		cardModal.style.display = "none";
-	}
-}
-
+  if (event.target === cardModal) {
+    cardModal.style.display = 'none';
+  }
+};
--- server/controllers/carsController.js
+++ server/controllers/carsController.js
@@ -15,31 +15,21 @@ export default class CarsController {
   */
   static addCarSaleAdvert(req, res) {
     const { body } = req;
-    const {
-      status, id, email, createdOn, manufacturer, model,
-    } = body;
-    if (!status || !id || !email || createdOn || !manufacturer || !model) {
-      res.json({
-        status: 404,
-        error: 'Please fill in the relevant details.',
-      });
-    } else {
-      // pick last advertisement and check its id
-      // The last advertisement's id + 1 is the new advertisement's id
+    // pick last advertisement and check its id
+    // The last advertisement's id + 1 is the new advertisement's id
 
-      const lastAdvertisement = advertisements.reverse()[0];
-      body.createdOn = faker.date.recent();
-      body.id = lastAdvertisement.id + 1;
-      advertisements.push(req.body);
-      res.json({
-        status: 200,
-        data: [
-          {
-            id: body.id,
-            message: 'Created Car sale asvertisement',
-          },
-        ],
-      });
-    }
+    const lastAdvertisement = advertisements.reverse()[0];
+    body.createdOn = faker.date.recent();
+    body.id = lastAdvertisement.id + 1;
+    advertisements.push(body);
+    res.send({
+      status: 200,
+      data: [
+        {
+          id: body.id,
+          message: 'Created Car sale advertisement',
+        },
+      ],
+    });
   }
 }
