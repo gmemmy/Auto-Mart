cb65fbfa255f4f1e2188f7a034ad17c17dab36a5
emmy <gmemmy22@gmail.com>
2019-06-07 02:37:17 +0100

    [Chore #166532209] Create data model with faker.js for Users

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
 import CarsController from '../controllers/carsController';
+import userController from '../controllers/userController';
+import validateSignup from '../middleware/validator';
 
 
 const routes = (app) => {
@@ -6,6 +8,9 @@ const routes = (app) => {
   app.get('/api/v1/carSales', CarsController.viewAllUnsoldCars);
   app.get('/api/v1/carSales/:id', CarsController.viewSpecificCar);
   app.post('/api/v1/carSales/purchase', CarsController.makePurchaseOrder);
+
+  // auth routes
+  app.post('api/v1/carSales/signup', validateSignup, userController.signup);
 
   return app;
 };
