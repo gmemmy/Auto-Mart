2ce99bc9016232cea0cf855f4918c298aa571d2c
Emmy <gmemmy22@gmail.com>
2019-06-13 21:32:32 +0100

    [Feature #166687377] Create API endpoint that allows the admin delete a specific car record, by id

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,6 +20,7 @@ const routes = (app) => {
 
   // admin routes
   app.get('/api/v1/admin', AdminController.viewAllCarRecords);
+  app.delete('/api/v1/admin/:id', AdminController.deleteASpecificRecord);
 
   return app;
 };
