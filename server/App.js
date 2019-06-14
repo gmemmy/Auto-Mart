f50cb0c92ea9a03405e9cd9f8f2b6136c9f6720a
Emmy <gmemmy22@gmail.com>
2019-06-14 22:11:32 +0100

    [Chore #166689003] Render swagger-UI in app.js

--- server/App.js
+++ server/App.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import swaggerUI from 'swagger-ui-express';
 import routes from './routes/index';
+import doc from '../doc.json';
 
 dotenv.config();
 
@@ -15,6 +17,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// render swagger UI
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(doc));
 routes(app);
 
 // wildcard route
