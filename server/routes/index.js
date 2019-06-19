08ad8f898e4d7176fa81a4212dc76be62edc04ba
Emmy <gmemmy22@gmail.com>
2019-06-19 01:05:31 +0100

    [Chore #166778932] Setup validation for creating new car adverts and validate password for sign up controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,23 @@
 import CarsController from '../controllers/carsController';
 // eslint-disable-next-line import/no-named-as-default
 import UserController from '../controllers/userController';
-import { validateSignup, validateSignin } from '../middleware/validator';
+import {
+  validateNewCarAdvert, checkPassword, validateSignup, validateSignin,
+} from '../middleware/validator';
 import AdminController from '../controllers/adminController';
 
 
 const routes = (app) => {
   app.get('/api/v1/carSales', CarsController.viewAllUnsoldCars);
   app.get('/api/v1/carSales/:id', CarsController.viewSpecificCar);
-  app.post('/api/v1/carSales', CarsController.addCarSaleAdvert);
+  app.post('/api/v1/carSales', validateNewCarAdvert, CarsController.addCarSaleAdvert);
   app.patch('/api/v1/carSales/:id/price', CarsController.updatePriceCarSaleAdvert);
   app.patch('/api/v1/carSales/:id/status', CarsController.updateStatusCarSaleAdvert);
   app.post('/api/v1/carSales/purchase', CarsController.makePurchaseOrder);
   app.patch('/api/v1/Carsales/:id/updatePurchase', CarsController.updatePricePurchaseOrder);
 
   // auth routes
-  app.post('/api/v1/auth/signup', validateSignup, UserController.signUp);
+  app.post('/api/v1/auth/signup', checkPassword, validateSignup, UserController.signUp);
   app.post('/api/v1/auth/signin', validateSignin, UserController.signIn);
 
   // admin routes
