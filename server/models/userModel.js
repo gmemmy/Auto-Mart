643ea445f7d034eb6e3d3628f0a3471c01c874c9
Emmy <gmemmy22@gmail.com>
2019-06-19 12:46:33 +0100

    [Chore #166714022] Setup User model to find user by email and by id

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -25,9 +25,51 @@ export default class UserModel {
       } = newUserObj;
 
       const query = `INSERT INTO Users(email, firstName, lastName, password, address, isAdmin) 
-        VALUES ('${email.trim()}, ${firstName.trim()}, ${lastName.trim()}, ${password.trim()}, ${address.trim()}, ${isAdmin},')
+        VALUES ('${email.trim()}', '${firstName.trim()}', '${lastName.trim()}', '${password.trim()}', '${address.trim()}', '${isAdmin},')
         RETURNING *
       `;
+
+      const response = await pool.query(query);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  /**
+ * @description - Sign
+ * @static
+ *
+ * @param {object} - email
+ *
+ * @memberof UserModel
+ *
+ * @returns {object} Class instance
+* */
+  static async getByEmail(email) {
+    try {
+      const query = `SELECT * FROM Users WHERE email = '${email.trim()}' `;
+
+      const response = await pool.query(query);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  /**
+* @description - Sign
+* @static
+*
+* @param {object} - User Id
+*
+* @memberof UserModel
+*
+* @returns {object} Class instance
+* */
+  static async getById(id) {
+    try {
+      const query = `SELECT * FROM Users WHERE id = '${id} `;
 
       const response = await pool.query(query);
       return response;
