635aa4dd03605e1bad3537dd903379097d68df22
Emmy <gmemmy22@gmail.com>
2019-07-07 12:20:19 +0100

    [Chore #166714022] Setup cloudinary

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
+import cloudinary from 'cloudinary';
 import routes from './routes/index';
 import doc from '../doc.json';
 
@@ -24,6 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // render swagger UI
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(doc));
 routes(app);
+
+// cloudinary setup
+cloudinary.uploader.upload();
 
 // wildcard route
 app.all('*', (req, res) => {
