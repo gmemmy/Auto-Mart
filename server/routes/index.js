2843b00d048d1426aaba59cfbe54ee3c9771fbff
gmemmy <gmemmy22@gmail.com>
2019-07-16 00:50:37 +0100

    #167287488-Auth-Bug-Fix (#76)
    * [Bug #167287488] Strip endpoints of all validation to fix Gradr error
    
    * [Chore #167287488] Validate password for authentication routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 import CarsController from '../controllers/carsController';
 // eslint-disable-next-line import/no-named-as-default
 import UserController from '../controllers/userController';
-// import {
-//   validateNewCarAdvert, checkPassword, validateSignup, validateSignin,
-// } from '../middleware/validator';
+import { validatePassword } from '../middleware/validator';
 // import authentication from '../middleware/userAuth';
 import AdminController from '../controllers/adminController';
 // import { userRecord, isAdmin } from '../middleware/userPermission';
@@ -22,12 +20,13 @@ const routes = (app) => {
   app.patch('/order/:order-id/price', CarsController.updatePricePurchaseOrder);
 
   // auth routes
-  app.post('/auth/signup', UserController.signUp);
-  app.post('/auth/signin', UserController.signIn);
+  app.post('/auth/signup', validatePassword, UserController.signUp);
+  app.post('/auth/signin', validatePassword, UserController.signIn);
 
   // admin routes
-  app.get('/car/', AdminController.viewAllCarRecords);
-  app.get('/users/', AdminController.viewAllUsers);
+  app.get('/api/v1/admin', AdminController.viewAllCarRecords);
+  app.get('/api/v1/admin/users', AdminController.viewAllUsers);
+
   app.delete('/car/:car_id/', AdminController.deleteASpecificRecord);
 
   return app;
