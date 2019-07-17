6397de5821713e1f406dbe2765e707a7cdfcd5e3
Emmy <gmemmy22@gmail.com>
2019-07-17 04:14:54 +0100

    [Bug #167287488 Remove unnecessary validation

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -66,7 +66,7 @@ export default class AdminController {
   */
   static async deleteASpecificRecord(req, res) {
     await CarModel.deleteById(req.params.id);
-    res.status(200).json({
+    return res.status(200).json({
       status: 200,
       data: 'Car advert successfully deleted',
     });
