8b47a0a4b1e5f6603dd6c93781dd6d2a9cceb2f3
Emmy <gmemmy22@gmail.com>
2019-07-17 04:59:20 +0100

    [Bug #167287488 Remove unnecessary validation

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -24,7 +24,7 @@ export const checkPassword = (req, res, next) => {
 
 export const validateGetCar = (req, res, next) => {
   const objectBody = req.body;
-  if (objectBody) {
+  if (!objectBody) {
     return res.status(400).json({
       status: 400,
       error: 'Please do not fill in any data',
