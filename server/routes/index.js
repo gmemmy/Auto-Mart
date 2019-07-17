f887489a77d0b58c74198e3af19fcfa9476758e9
Emmy <gmemmy22@gmail.com>
2019-07-17 04:31:31 +0100

    [Bug #167287488 Remove unnecessary validation

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,16 +2,17 @@ import CarsController from '../controllers/carsController';
 // eslint-disable-next-line import/no-named-as-default
 import UserController from '../controllers/userController';
 import {
-  errorSignupBody, errorSigninBody, errorNewCar, errorNewOrder, validateGetCar,
+  errorSignupBody, errorSigninBody, errorNewCar, errorNewOrder,
   errorUpdatePriceOfCar, errorUpdateStatusOfCar, errorUpdatePriceOfOrder,
 } from '../middleware/validator';
 // import authentication from '../middleware/userAuth';
 import AdminController from '../controllers/adminController';
+import authentication from '../middleware/userAuth';
 // import { userRecord, isAdmin } from '../middleware/userPermission';
 
 
 const routes = (app) => {
-  app.get('/car/', validateGetCar, CarsController.viewAllUnsoldCars);
+  app.get('/car/', authentication, CarsController.viewAllUnsoldCars);
   // app.get('/car/', CarsController.viewAllUnsoldCarsWithinAPriceRange);
   // app.get('/api/v1/carSales/bodyType', CarsController.viewAllUnsoldCarsOfSpecificBodyType);
   // app.get('/api/v1/carSales/unsold/used', CarsController.viewAllUnsoldCarsofUsedState);
